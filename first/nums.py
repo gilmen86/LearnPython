239fc9ac8d04f4031b2aaf7fe0071595e844c199
gilmen86 <gilmen198619861986@gmail.com>
2022-07-28 06:58:08 +0300

    закончил с задачами строк. Понравилось выравнивание. Будет полезно

--- first/nums.py
+++ first/nums.py
@@ -7,5 +7,38 @@
 #     print(minus(2, 2))
 # print('Вроде все')
 
-number_pi = 3.141592653589793
-print(f'{number_pi:.6f}')
+# 2.8
+# number_pi = 3.141592653589793
+# print(f'{number_pi:.6f}')
+
+# i=float(input())
+# print(f'{i:.2f}')
+
+# i=int(input())
+# print(f'{i:010d}')
+
+# Выравниванеи, заполнение
+# APPLES = .50
+# BREAD = 1.50
+# CHEESE = 2.25
+# num_apples = 3
+# num_bread = 10
+# num_cheese = 6
+# price_apples = num_apples * APPLES
+# price_bread = num_bread * BREAD
+# price_cheese = num_cheese * CHEESE
+# str_apples = 'Яблоки'
+# str_bread = 'Хлеб'
+# str_сheese = 'Сыр'
+# total = price_bread + price_cheese + price_apples
+# print(f'{"Список покупок":^30s}')
+# print(f'{"=" * 30}')
+# print(f'{str_apples:10s}{num_apples:10d}\t${price_apples:>.2f}')
+# print(f'{str_bread:10s}{num_bread:10d}\t${price_bread:>5.2f}')
+# print(f'{str_сheese:10s}{num_cheese:10d}\t${price_cheese:>5.2f}')
+# print(f'{"Total:":>20s}\t${total:>5.2f}')
+
+text = input()
+print(f"|{text:&^20}|")
+print(f"|{text:&>20}|")
+print(f"|{text:&<20}|")
