90f9b4a352fc2b39a41cd315833e3eeb53da074e
gilmen86 <gilmen198619861986@gmail.com>
2022-07-28 06:12:12 +0300

    Велл задачу 2.8 (6 знаков после запятой)

--- first/nums.py
+++ first/nums.py
@@ -1,9 +1,11 @@
-def plus(a,b):
-    return a+b
-def minus (a,b):
-    return a-b
-if __name__=='__main__':
-    print(plus(2,2))
-    print(minus(2, 2))
-print('Вроде все')
+# def plus(a,b):
+#     return a+b
+# def minus (a,b):
+#     return a-b
+# if __name__=='__main__':
+#     print(plus(2,2))
+#     print(minus(2, 2))
+# print('Вроде все')
 
+number_pi = 3.141592653589793
+print(f'{number_pi:.6f}')
